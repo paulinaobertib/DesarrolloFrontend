caa1e15b10b6088f5a8bd2219d702362eea516b4
Vicky Vaccarini <vicky_vaccarini@hotmail.com>
2025-09-11 14:21:57 -0300

    Se acomodo el form y se separaron los estilos

--- clase0309/src/app/curriculum/projects-grid.ts
+++ clase0309/src/app/curriculum/projects-grid.ts
@@ -1,13 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { NgFor } from '@angular/common';
 
-type Project = { title: string; img: string; cssClass: string; };
+export type Project = { title: string; img?: string; cssClass?: string };
 
 @Component({
   selector: 'app-projects-grid',
   standalone: true,
-  imports: [NgFor],
   templateUrl: './projects-grid.html',
+  styleUrls: ['./projects-grid.scss'], 
 })
 export class ProjectsGrid {
   @Input({ required: true }) items: Project[] = [];
