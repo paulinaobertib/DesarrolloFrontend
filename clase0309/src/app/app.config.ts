3b1c0f51d72ea0c08d9bc8899782b37a0bdeabf6
Vicky Vaccarini <vicky_vaccarini@hotmail.com>
2025-09-18 20:44:36 -0300

    Se agrego lo de la galeria de arte y se modifico el estilo de las cards

--- clase0309/src/app/app.config.ts
+++ clase0309/src/app/app.config.ts
@@ -1,15 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './misc/http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        anchorScrolling: 'enabled',
-        scrollPositionRestoration: 'enabled',
-      })
-    ),
-  ],
+    provideHttpClient(withInterceptors([httpInterceptor])),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes)
+  ]
 };
--- clase0309/src/app/app.routes.ts
+++ clase0309/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Curriculum } from './components/curriculum/curriculum';
 import { Form } from './components/form/form';
 import { authGuard } from './misc/auth-guard';
+import { ServiceHttpComponent } from './components/service-http/service-http-component';
 
 export const routes: Routes = [
   { path: '', 
@@ -9,6 +10,11 @@ export const routes: Routes = [
   { path: 'contacto', 
     component: Form,
     canActivate: [authGuard]},
+  {
+    path: 'service-http-component',
+    component: ServiceHttpComponent,
+    canActivate: [authGuard],
+  },
   { path: '**', 
     redirectTo: '' }, 
 ];
